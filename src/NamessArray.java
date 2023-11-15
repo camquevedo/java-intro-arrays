17ca2738867b801713a301d3490084962d872132
Camilo Quevedo <camquevedo@hotmail.com>
2023-11-14 19:30:35 -0500

    :loud_sound: add a remote commit

--- src/NamessArray.java
+++ src/NamessArray.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 
 public class NamessArray {
-    //registro de un comentario añadido desde localg
+    //registro de un comentario añadido desde local
+    // pero no me gusto, te chingas
 
     public static void main(String[] args) {
         // 1. Crear un array de cadenas llamado `nombres` con un tamaño de 5.
